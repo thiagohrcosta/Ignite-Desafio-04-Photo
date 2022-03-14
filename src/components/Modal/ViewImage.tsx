d4febbff71aeb4c75143150bf781440407aa35e3
Thiago Costa <thiagohrcosta86@gmail.com>
2022-03-14 14:07:04 -0300

    refactoring view image

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -1,12 +1,12 @@
-import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalFooter,
-  ModalBody,
-  Image,
-  Link,
-} from '@chakra-ui/react';
+import { 
+  Image, 
+  Link, 
+  Modal, 
+  ModalBody, 
+  ModalContent, 
+  ModalFooter, 
+  ModalOverlay 
+} from "@chakra-ui/react";
 
 interface ModalViewImageProps {
   isOpen: boolean;
@@ -19,5 +19,24 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent bgColor="pGray.800" w="fit-content" maxW="full">
+        <ModalBody w="fit-content" maxW="full" p={0}>
+          <Image 
+            src={imgUrl} 
+            alt="" maxW={900} 
+            maxH={600} 
+            objectFit="cover" 
+          />
+        </ModalBody>
+        <ModalFooter>
+          <Link href={imgUrl} target="_blank" mr="auto">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
